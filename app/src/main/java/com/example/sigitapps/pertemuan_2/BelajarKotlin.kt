dd5bdca5925fc66831a2aa8e02e39d3a1c2aed01
ilman <ilman24si@mahasiswa.pcr.ac.id>
2026-03-11 16:00:03 +0700

    First Commit

---
+++ app/src/main/java/com/example/sigitapps/pertemuan_2/BelajarKotlin.kt
@@ -0,0 +1,45 @@
+package com.example.sigitapps.pertemuan_2
+
+fun main(){
+    println("Hai rekan-rekan...")
+    println("Selamat datang di bahasa pemrograman Kotlin")
+
+    println("==============")
+
+    var angka = 15
+    println("Hasil dari 15 + 10 =${angka + 10}")
+
+    var nilaiInt = 10000
+    var nilaiDouble = 100.003
+    var nilaiFloat = 1000.0f
+
+    println("Nilai Integer = $nilaiInt")
+    println("Nilai Double = $nilaiDouble")
+    println("Nilai Float = $nilaiFloat")
+
+    println("====== STRING ======")
+    val huruf = 'a'
+    println("Ini penggunaan karakter = $huruf'")
+
+    val nilaiString = "Mawar"
+    println("Halo $nilaiString!\nApa kabar?")
+
+    println("========== KONDISI ========")
+
+    val nilai = 10
+    if(nilai<0)
+        println("Bilangan negatif")
+    else {
+        if(nilai%2 == 0)
+            println("Bilangan Genap")
+        else
+            println("Bilangan Ganjil")
+
+    }
+
+    println("======== Perulangan ========")
+    val KampusKu: Array<String> = arrayOf("Kampus", "Politeknik", "Caltex", "Riau")
+    for (kampus: String in KampusKu){
+        println(kampus)
+    }
+}
