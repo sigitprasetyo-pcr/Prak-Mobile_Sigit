574b08261e96d270950b146cce6b87ea2fca863d
ilman <ilman24si@mahasiswa.pcr.ac.id>
2026-04-01 16:40:06 +0700

    pertemuan3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,17 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
+
 
 dependencies {
     implementation(libs.androidx.core.ktx)
